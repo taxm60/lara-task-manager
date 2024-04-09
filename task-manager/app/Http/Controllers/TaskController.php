37f2a663f97ab510dc9d22675b4b8f23fdec54d8
taxm60 <157100645+taxm60@users.noreply.github.com>
2024-04-09 10:04:31 +0000

    apiResource route

--- task-manager/app/Http/Controllers/TaskController.php
+++ task-manager/app/Http/Controllers/TaskController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index (Request $req)
+    public function index(Request $req)
     {
         //return response()->json( Task::all() );
         return new TaskCollection(Task::all());
+    }
+
+    public function show(Request $req, Task $task)
+    {
+        return new TaskResource($task);
     }
 }
